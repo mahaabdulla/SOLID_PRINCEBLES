bca1235de54cee1890a5dfa4e7941438d58848b3
mahaabdulla <mahaabdulla87@gmail.com>
2025-06-06 23:45:21 +0300

    add weight var

---
+++ preson.dart
@@ -0,0 +1,13 @@
+class Preson {
+  int? id, height, weight;
+
+  // methods
+  changeHeight(int height) {
+    if (height <= 0) print("Plz enter  positve number + ");
+  } // validation to value i dont wana - values
+
+  changeWeight() {} // validation to value i dont wana - values
+  bmi() {} // cala bmi weight/h2
+  saveBmi() {}
+  getBmiHistory() {}
+}
